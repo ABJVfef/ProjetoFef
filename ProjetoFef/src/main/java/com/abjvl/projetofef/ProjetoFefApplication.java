c72100cdfecfb0cb3bff38a636298e9303293c38
Breno Rogerio <brenonovorogerio@gmail.com>
2025-10-23 21:29:40 -0300

    criação do Projeto

---
+++ ProjetoFef/src/main/java/com/abjvl/projetofef/ProjetoFefApplication.java
@@ -0,0 +1,13 @@
+package com.abjvl.projetofef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFefApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetoFefApplication.class, args);
+    }
+
+}
---
+++ ProjetoFef/src/test/java/com/abjvl/projetofef/ProjetoFefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abjvl.projetofef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFefApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
